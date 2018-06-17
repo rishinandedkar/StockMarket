a23618629519d3f2c1cb29ec18cb5cbe37fdfe12
Rishi_Nandedkar <nandedkar.r@husky.neu.edu>
2018-06-16 21:18:34 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent,
   ],
   imports: [
     BrowserModule
